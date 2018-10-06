ec88fdb76f4e418ede26393211ec6a2b0d3479f9
tkaaad97 <tkaaad97@gmail.com>
2018-10-06 18:12:24 +0900

    render with template engine

--- app/CFn.hs
+++ app/CFn.hs
@@ -6,7 +6,10 @@ module CFn
     , PropertyType(..)
     , Property(..)
     , PropertyTypeDefinition(..)
+    , PropertyTypeDefinitions(..)
     , AttributeType(..)
+    , Attribute(..)
+    , Attributes(..)
     , ResourceTypeDefinition(..)
     , Specification(..)
     ) where
@@ -16,19 +19,21 @@ import qualified Data.Aeson as DA (FromJSON, ToJSON, Value(..), object,
                                    parseJSON, toJSON, withObject)
 import qualified Data.Aeson.TH as DA (Options(..), SumEncoding(..),
                                       defaultOptions, deriveJSON)
+import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 import Data.Text (Text)
+import qualified Data.Text as Text
 
 data PrimitiveType =
-    CFnBoolean |
-    CFnDouble |
-    CFnInteger |
-    CFnJson |
-    CFnLong |
-    CFnString |
-    CFnTimestamp
+    PrimBoolean |
+    PrimDouble |
+    PrimInteger |
+    PrimJson |
+    PrimLong |
+    PrimString |
+    PrimTimestamp
     deriving (Show, Eq)
-$(DA.deriveJSON DA.defaultOptions { DA.constructorTagModifier = drop 3, DA.sumEncoding = DA.UntaggedValue } ''PrimitiveType)
+$(DA.deriveJSON DA.defaultOptions { DA.constructorTagModifier = drop 4, DA.sumEncoding = DA.UntaggedValue } ''PrimitiveType)
 
 data UpdateType =
     UpdateTypeImmutable |
@@ -45,24 +50,21 @@ data PropertyType =
     deriving (Show, Eq)
 
 data Property = Property
-    { propertyName         :: !Text
-    , propertyType         :: !PropertyType
+    { propertyType         :: !PropertyType
     , propertyDocumentaion :: !Text
     , propertyRequired     :: !Bool
     , propertyUpdateType   :: !UpdateType
     } deriving (Show, Eq)
 
-newtype Properties = Properties [Property]
-    deriving (Show, Eq)
-
 data PropertyTypeDefinition = PropertyTypeDefinition
-    { propertyTypeDefinitionName          :: !Text
+    { propertyTypeDefinitionNameSpace     :: !Text
+    , propertyTypeDefinitionName          :: !Text
     , propertyTypeDefinitionDocumentation :: !Text
-    , propertyTypeDefinitionProperties    :: !Properties
+    , propertyTypeDefinitionProperties    :: !(HashMap Text Property)
     } deriving (Show, Eq)
-$(DA.deriveJSON DA.defaultOptions { DA.constructorTagModifier = drop 22, DA.sumEncoding = DA.UntaggedValue } ''PropertyTypeDefinition)
 
-newtype PropertyTypeDefinitions = PropertyTypeDefinitions [PropertyTypeDefinition]
+newtype PropertyTypeDefinitions = PropertyTypeDefinitions
+    { unPropertyTypeDefinitions :: HashMap Text [PropertyTypeDefinition] }
     deriving (Show, Eq)
 
 data AttributeType =
@@ -70,136 +72,137 @@ data AttributeType =
     AttributeTypePrimitiveList !PrimitiveType
     deriving (Show, Eq)
 
-data Attribute = Attribute
-    { attributeName :: !Text
-    , attributeType :: !AttributeType
+newtype Attribute = Attribute
+    { attributeType :: AttributeType
     } deriving (Show, Eq)
 
-newtype Attributes = Attributes [Attribute]
+newtype Attributes = Attributes
+    { unAttributes :: [Attribute] }
     deriving (Show, Eq)
 
 data ResourceTypeDefinition = ResourceTypeDefinition
-    { resourceTypeDefinitionName          :: !Text
-    , resourceTypeDefinitionDocumentation :: !Text
-    , resourceTypeDefinitionAttributes    :: ![Attribute]
-    , resourceTypeDefinitionProperties    :: !Properties
+    { resourceTypeDefinitionDocumentation :: !Text
+    , resourceTypeDefinitionAttributes    :: !(Maybe (HashMap Text Attribute))
+    , resourceTypeDefinitionProperties    :: !(HashMap Text Property)
     } deriving (Show, Eq)
-$(DA.deriveJSON DA.defaultOptions { DA.constructorTagModifier = drop 22, DA.sumEncoding = DA.UntaggedValue } ''ResourceTypeDefinition)
-
-newtype ResourceTypeDefinitions = ResourceTypeDefinitions [ResourceTypeDefinition]
-    deriving (Show, Eq)
+$(DA.deriveJSON DA.defaultOptions { DA.fieldLabelModifier = drop 22, DA.sumEncoding = DA.UntaggedValue } ''ResourceTypeDefinition)
 
 data Specification = Specification
     { specificationPropertyTypes                :: !PropertyTypeDefinitions
-    , specificationResourceTypes                :: !ResourceTypeDefinitions
+    , specificationResourceTypes                :: !(HashMap Text ResourceTypeDefinition)
     , specificationResourceSpecificationVersion :: !Text
     } deriving (Show, Eq)
-$(DA.deriveJSON DA.defaultOptions { DA.constructorTagModifier = drop 13, DA.sumEncoding = DA.UntaggedValue } ''Specification)
+$(DA.deriveJSON DA.defaultOptions { DA.fieldLabelModifier = drop 13, DA.sumEncoding = DA.UntaggedValue } ''Specification)
 
-instance DA.FromJSON Properties where
-    parseJSON = DA.withObject "Properties" $ fmap Properties . HM.foldrWithKey f (return [])
+instance DA.FromJSON Property where
+    parseJSON = DA.withObject "Properties" $ \v ->
+        let t = HM.lookup "Type" v
+            item = HM.lookup "ItemType" v
+            pitem = HM.lookup "PrimitiveItemType" v
+
+        in case (t, item, pitem) of
+            (Just (DA.String "List"), Just (DA.String a), _) -> parseResourceList v a
+            (Just (DA.String "List"), Nothing, Just a @ DA.String {}) -> parsePrimitiveList v =<< DA.parseJSON a
+            (Just (DA.String t), _, _) -> parseResource v t
+            (Nothing, _, _) -> parsePrimitive v =<< v .: "PrimitiveType"
 
         where
-        f n v a = (:) <$> DA.withObject "Property" (parse n) v <*> a
-
-        parse n v =
-            let t = HM.lookup "Type" v
-                item = HM.lookup "ItemType" v
-                pitem = HM.lookup "PrimitiveItemType" v
-
-            in case (t, item, pitem) of
-                (Just (DA.String "List"), Just (DA.String a), _) -> parseResourceList n v a
-                (Just (DA.String "List"), Nothing, Just a @ DA.String {}) -> parsePrimitiveList n v =<< DA.parseJSON a
-                (Just (DA.String t), _, _) -> parseResource n v t
-                (Nothing, _, _) -> parsePrimitive n v =<< v .: "PrimitiveType"
-
-        parseResource n v a = Property n (PropertyType a)
+        parseResource v a = Property (PropertyType a)
             <$> v .: "Documentation"
             <*> v .: "Required"
             <*> v .: "UpdateType"
 
-        parsePrimitive n v a = Property n (PropertyTypePrimitive a)
+        parsePrimitive v a = Property (PropertyTypePrimitive a)
             <$> v .: "Documentation"
             <*> v .: "Required"
             <*> v .: "UpdateType"
 
-        parseResourceList n v a = Property n (PropertyTypeList a)
+        parseResourceList v a = Property (PropertyTypeList a)
             <$> v .: "Documentation"
             <*> v .: "Required"
             <*> v .: "UpdateType"
 
-        parsePrimitiveList n v a = Property n (PropertyTypePrimitiveList a)
+        parsePrimitiveList v a = Property (PropertyTypePrimitiveList a)
             <$> v .: "Documentation"
             <*> v .: "Required"
             <*> v .: "UpdateType"
 
-instance DA.ToJSON Properties where
-    toJSON (Properties ps) = DA.Object . HM.fromList $ map f ps
+instance DA.ToJSON Property where
+    toJSON (Property (PropertyType a) doc required update) =
+        DA.object
+        [ "Type" .= a
+        , "Documentation" .= doc
+        , "Required" .= required
+        , "UpdateType" .= update
+        ]
+
+    toJSON (Property (PropertyTypePrimitive a) doc required update) =
+        DA.object
+        [ "PrimitiveType" .= a
+        , "Documentation" .= doc
+        , "Required" .= required
+        , "UpdateType" .= update
+        ]
+
+    toJSON (Property (PropertyTypeList a) doc required update) =
+        DA.object
+        [ "Type" .= DA.String "List"
+        , "ItemType" .= a
+        , "Documentation" .= doc
+        , "Required" .= required
+        , "UpdateType" .= update
+        ]
+
+    toJSON (Property (PropertyTypePrimitiveList a) doc required update) =
+        DA.object
+        [ "Type" .= DA.String "List"
+        , "PrimitiveItemType" .= a
+        , "Documentation" .= doc
+        , "Required" .= required
+        , "UpdateType" .= update
+        ]
+
+instance DA.FromJSON Attribute where
+    parseJSON = DA.withObject "Attributes" $ \v ->
+        let pitem = HM.lookup "PrimitiveItemType" v
+        in case pitem of
+            Just a  -> Attribute . AttributeTypePrimitiveList <$> DA.parseJSON a
+            Nothing -> Attribute . AttributeTypePrimitive <$> v .: "PrimitiveType"
+
+instance DA.ToJSON Attribute where
+    toJSON (Attribute (AttributeTypePrimitive a)) =
+        DA.object
+        [ "PrimitiveType" .= a
+        ]
+
+    toJSON (Attribute (AttributeTypePrimitiveList a)) =
+        DA.object
+        [ "Type" .= DA.String "List"
+        , "PrimitiveItemType" .= a
+        ]
+
+instance DA.FromJSON PropertyTypeDefinitions where
+    parseJSON = DA.withObject "PropertyTypeDefinitions" $ fmap PropertyTypeDefinitions . HM.foldrWithKey f (return HM.empty)
 
         where
 
-        f p = (propertyName p, toJSON' p)
+        f k v m =
+            let (ns, n') = Text.breakOn "." k
+                n = Text.drop 1 n'
+                a = return <$> DA.withObject "PropertyTypeDefinition" (parse ns n) v
+            in HM.insertWith (++) ns <$> a <*> m
 
-        toJSON' (Property _ (PropertyType a) doc required update) =
-            DA.object
-            [ "Type" .= a
-            , "Documentation" .= doc
-            , "Required" .= required
-            , "UpdateType" .= update
-            ]
+        parse ns n v = PropertyTypeDefinition ns n
+            <$> v .: "Documentation"
+            <*> v .: "Properties"
 
-        toJSON' (Property _ (PropertyTypePrimitive a) doc required update) =
-            DA.object
-            [ "PrimitiveType" .= a
-            , "Documentation" .= doc
-            , "Required" .= required
-            , "UpdateType" .= update
-            ]
-
-        toJSON' (Property _ (PropertyTypeList a) doc required update) =
-            DA.object
-            [ "Type" .= DA.String "List"
-            , "ItemType" .= a
-            , "Documentation" .= doc
-            , "Required" .= required
-            , "UpdateType" .= update
-            ]
-
-        toJSON' (Property _ (PropertyTypePrimitiveList a) doc required update) =
-            DA.object
-            [ "Type" .= DA.String "List"
-            , "PrimitiveItemType" .= a
-            , "Documentation" .= doc
-            , "Required" .= required
-            , "UpdateType" .= update
-            ]
-
-instance DA.FromJSON Attributes where
-    parseJSON = DA.withObject "Attributes" $ fmap Attributes . HM.foldrWithKey f (return [])
-
+instance DA.ToJSON PropertyTypeDefinitions where
+    toJSON (PropertyTypeDefinitions ds) = DA.object . map f . concat . HM.elems $ ds
         where
-
-        f n v a = (:) <$> DA.withObject "Attribute" (parse n) v <*> a
-
-        parse n v =
-            let pitem = HM.lookup "PrimitiveItemType" v
-            in case pitem of
-                Just a  -> Attribute n . AttributeTypePrimitiveList <$> DA.parseJSON a
-                Nothing -> Attribute n . AttributeTypePrimitive <$> v .: "PrimitiveType"
-
-instance DA.ToJSON Attributes where
-    toJSON (Attributes xs) = DA.Object . HM.fromList $ map f xs
-
-        where
-        f a = (attributeName a, toJSON' a)
-
-        toJSON' (Attribute _ (AttributeTypePrimitive a)) =
-            DA.object
-            [ "PrimitiveType" .= a
-            ]
-
-        toJSON' (Attribute _ (AttributeTypePrimitiveList a)) =
-            DA.object
-            [ "Type" .= DA.String "List"
-            , "PrimitiveItemType" .= a
-            ]
+        f (PropertyTypeDefinition ns n doc ps) =
+            let k = Text.concat [ns, ".", n]
+                v = DA.object
+                    [ "Documentations" .= doc
+                    , "Properties" .= DA.toJSON ps
+                    ]
+            in (k, v)
--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,110 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import CFn (Specification)
+import CFn (Attribute(..), AttributeType(..), PrimitiveType(..), Property(..),
+            PropertyType(..), PropertyTypeDefinition(..),
+            PropertyTypeDefinitions(..), ResourceTypeDefinition(..),
+            Specification(..))
+import qualified Data.ByteString as BS (readFile)
 import qualified Data.ByteString.Lazy as BSL (toStrict)
+import Data.Maybe (maybe)
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text (putStr, readFile)
+import qualified Data.Text.Lazy as TL (toStrict)
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types.Status (statusCode)
 
-import Data.Aeson (eitherDecodeStrict)
+import Data.Aeson (eitherDecodeStrict, (.=))
+import qualified Data.Aeson as DA (object)
+import qualified Data.HashMap.Strict as HM
+import qualified Text.EDE as EDE (eitherParseFile, eitherRender, fromPairs)
 
 main :: IO ()
 main = do
-  manager <- newManager tlsManagerSettings
+    {-
+    manager <- newManager tlsManagerSettings
 
-  request <- parseRequest "https://d33vqc0rt9ld30.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json"
-  response <- httpLbs request manager
+    request <- parseRequest "https://d33vqc0rt9ld30.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json"
+    response <- httpLbs request manager
 
-  putStrLn $ "The status code was: " ++ show (statusCode $ responseStatus response)
-  let x = eitherDecodeStrict . BSL.toStrict $ responseBody response
-  print (x :: Either String Specification)
+    putStrLn $ "The status code was: " ++ show (statusCode $ responseStatus response)
+    let x = eitherDecodeStrict . BSL.toStrict $ responseBody response
+    print (x :: Either String Specification)
+    -}
+    source <- BS.readFile "resource-specification.json"
+    spec <- either error return $ eitherDecodeStrict source
+    let propertyDefinitions = unPropertyTypeDefinitions . specificationPropertyTypes $ spec
+        resourceDefinitions = specificationResourceTypes spec
+
+    t <- EDE.eitherParseFile "template/code.ede"
+
+    mapM_ (renderResource t propertyDefinitions) . HM.toList $ resourceDefinitions
+
+    where
+
+    renderPropertyDefinitions t (k, ds) = do
+        let env = EDE.fromPairs
+                [ "Module" .= Text.replace "::" "." k
+                , "PropertyDefinitions" .= map propertyDefToJSON ds
+                ]
+        either error (Text.putStr . TL.toStrict)  $ t >>= (`EDE.eitherRender` env)
+
+    renderResource t ps (k, resource) =  do
+        let ds = HM.lookupDefault [] k ps
+            env = EDE.fromPairs
+                [ "Module" .= Text.replace "::" "." k
+                , "ResourceName" .= snd (Text.breakOnEnd "::" k)
+                , "PropertyDefinitions" .= map propertyDefToJSON ds
+                , "ResourceDefinition" .= resourceToJSON resource
+                ]
+        either error (Text.putStr . TL.toStrict)  $ t >>= (`EDE.eitherRender` env)
+
+    renderType True (PropertyType t) = t
+    renderType False (PropertyType t) = Text.concat ["Maybe ", t]
+    renderType True (PropertyTypePrimitive t) = renderPrimitiveType t
+    renderType False (PropertyTypePrimitive t) = Text.concat ["Maybe ", renderPrimitiveType t]
+    renderType True (PropertyTypeList t) = Text.concat ["[", t, "]"]
+    renderType False (PropertyTypeList t) = Text.concat ["Maybe [", t, "]"]
+    renderType True (PropertyTypePrimitiveList t) = Text.concat ["[", renderPrimitiveType t, "]"]
+    renderType False (PropertyTypePrimitiveList t) = Text.concat ["Maybe [", renderPrimitiveType t, "]"]
+
+    renderPrimitiveType PrimBoolean   = "Bool"
+    renderPrimitiveType PrimDouble    = "Double"
+    renderPrimitiveType PrimInteger   = "Int"
+    renderPrimitiveType PrimJson      = "Value"
+    renderPrimitiveType PrimLong      = "Integer"
+    renderPrimitiveType PrimString    = "Text"
+    renderPrimitiveType PrimTimestamp = "Text"
+
+    renderAttributeType (AttributeTypePrimitive t) = renderPrimitiveType t
+    renderAttributeType (AttributeTypePrimitiveList t) = Text.concat ["[", renderPrimitiveType t, "]"]
+
+    attributeToJSON (k, Attribute t) =
+        DA.object
+        [ "Name" .= k
+        , "Type" .= renderAttributeType t
+        ]
+
+    propertyToJSON (k, Property t doc required update) =
+        DA.object
+        [ "Name" .= k
+        , "Type" .= renderType required t
+        , "Reuqired" .= required
+        , "UpdateType" .= update
+        ]
+    propertyDefToJSON (PropertyTypeDefinition ns n doc ps) =
+        DA.object
+        [ "Name" .= n
+        , "NameSpace" .= ns
+        , "Documentation" .= doc
+        , "Properties" .= map propertyToJSON (HM.toList ps)
+        ]
+
+    resourceToJSON (ResourceTypeDefinition doc attributes properties) =
+        DA.object
+        [ "Documentation" .= doc
+        , "Attributes" .= maybe [] (map attributeToJSON . HM.toList) attributes
+        , "Properties" .= map propertyToJSON (HM.toList properties)
+        ]
